d5087a3ede1690b8a2cabb2fa02e66fa1254cf4c
Biscode <bisma.tariq98@gmail.com>
2021-06-25 23:40:57 +0500

    newmmm

--- lib/storage_path.dart
+++ lib/storage_path.dart
@@ -5,21 +5,21 @@ import 'package:flutter/services.dart';
 class StoragePath {
   static const MethodChannel _channel = const MethodChannel('storage_path');
 
-  static  get imagesPath async {
+  static Future<String> get imagesPath async {
     final String data = await _channel.invokeMethod('getImagesPath');
     return data;
   }
 
-  static  get videoPath async {
+  static Future<String> get videoPath async {
     final String data = await _channel.invokeMethod('getVideosPath');
     return data;
   }
 
-  static  get audioPath async {
+  static Future<String> get audioPath async {
     final String data = await _channel.invokeMethod('getAudioPath');
     return data;
   }
-  static  get filePath async {
+  static Future<String> get filePath async {
     final String data = await _channel.invokeMethod('getFilesPath');
     return data;
   }
